8b5d932e1cf0b4b39691db6ee7e09e560cf722fc
ClintonTak <clintontak22@live.com>
2017-07-01 00:25:19 -0400

    used js to load the whole page on the index
    removed about, contact, and resume individual pages, used hide and show
    js to have them all be on index.

--- js/java.js
+++ js/java.js
@@ -1,1 +1,38 @@
-java.j
+$(document).ready(function(){
+	$("#home").show();
+    $("#about").hide();
+    $("#resume").hide();
+    $("#contact").hide();
+
+    $("homeButton").click(function(){
+        $("#home").show();
+        $("#about").hide();
+        $("#resume").hide();
+        $("#contact").hide();
+    });
+
+    $("aboutButton").click(function(){
+        $("#home").hide();
+        $("#about").show();
+        $("#resume").hide();
+        $("#contact").hide();
+    });
+
+    $("ResumeButton").click(function(){
+        $("#home").hide();
+        $("#about").hide();
+        $("#resume").show();
+        $("#contact").hide();
+    });
+
+    $("contactButton").click(function(){
+        $("#home").hide();
+        $("#about").hide();
+        $("#resume").hide();
+        $("#contact").show();
+    });
+
+    
+});
+
+
